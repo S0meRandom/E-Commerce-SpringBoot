b19d77f4794eddab103c52c0b01a9f6af0d409cd
Kamil Mazur <kamilosmaz@gmail.com>
2026-03-10 15:22:12 +0100

    Poprawiono wizualnie strone, dodano Spring Security, dodano możliwość Zarejestrowania konta i zalogowania się.

--- src/main/java/com/kam/e_com/Config/SecurityConfig.java
+++ src/main/java/com/kam/e_com/Config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +14,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Wyłączamy ochronę CSRF (ważne przy testowaniu POST)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Pozwól na absolutnie wszystko (tymczasowo!)
-                );
+        http.
+                csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/login.html","/login.js","/register.html","/register.js","/api/auth/register")
+                        .permitAll().anyRequest().authenticated())
+                .formLogin(form->form
+                        .loginPage("/login.html")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/index.html",true)
+                        .failureUrl("/login.html")
+                        .permitAll())
+                .logout(logout-> logout
+                        .logoutSuccessUrl("/login.html")
+                        .permitAll());
         return http.build();
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
