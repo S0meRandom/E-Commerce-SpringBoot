afbc83278264160f08cd3a364b653048a485aff0
Kamil Mazur <kamilosmaz@gmail.com>
2026-03-19 11:15:49 +0100

    Zmodyfikowano modal edytowania ofert, połączono z backendem, zmieniono wygląd przycisków w index.html

--- src/main/resources/static/userProfile/userOffers/userOffers.js
+++ src/main/resources/static/userProfile/userOffers/userOffers.js
@@ -3,22 +3,21 @@ async function getUserData(){
         const response = await fetch("/api/auth/me")
         if(response.ok){
             const userData = await response.json();
-
             const user_id = userData.id;
-
             await fetchUserOffers(user_id);
-
 
         }
     }catch(error){
         console.error("Błąd pobierania danych"+error)
     }
 }
+
+let userOffers = [];
 async function fetchUserOffers(user_id){
     try{
         const response = await fetch(`/api/products/seller/${user_id}`)
         if(response.ok){
-            const userOffers = await response.json();
+            userOffers = await response.json();
             const userOffersCount = userOffers.length;
             document.getElementById('count-value').innerText = userOffersCount;
             document.getElementById('total-price').innerText = `${calculateValue(userOffers)} zł`;
@@ -26,7 +25,6 @@ async function fetchUserOffers(user_id){
         }
 
     }catch(error){
-
     }
 
 }
@@ -42,7 +40,7 @@ function renderOffers(userOffers){
         <p>${offer.description}</p>
         <button onclick="deleteProduct(${offer.id})">Usuń Oferte</button>
         <button onclick="location.href='/product-page/product-page.html?id=${offer.id}'">Zobacz Oferte</button>
-        <button onclick="editOffer(${offer.id})">Edytuj Oferte</button>
+        <button onclick="showModal(${offer.id})">Edytuj Oferte</button>
         </div>`;
 
         container.innerHTML += productHTML;
@@ -69,16 +67,40 @@ function calculateValue(offers){
     return sum;
 }
 
-function editOffer(){
+function showModal(offerId){
     const modal = document.getElementById("editModal");
     modal.style.display = "block";
-
-
+    const offer = userOffers.find(o=>String(offerId)===String(o.id));
+    document.getElementById('modalPrice').value = offer.price;
+    document.getElementById('modalName').value = offer.name;
+    document.getElementById('modalDescription').value = offer.description;
+    document.getElementById('modalStock_quantity').value = offer.stock_quantity;
+    document.getElementById('modalSave-btn').onclick = () =>editUserOffer(offerId);
 
 }
 function closeModal(){
     const modal= document.getElementById('editModal');
     modal.style.display = "none";
+}
+async function editUserOffer(id) {
+    const offer = {
+        price: document.getElementById('modalPrice').value,
+        name: document.getElementById('modalName').value,
+        description: document.getElementById('modalDescription').value,
+        stock_quantity: document.getElementById('modalStock_quantity').value
+    }
+
+    const response = await fetch(`/api/products/${id}`,{
+        method: 'PUT',
+        body : JSON.stringify(offer),
+        headers: {'Content-Type': 'application/json'}
+    });
+    if(response.ok){
+        alert("Zapisano zmiany oferty");
+    }else{
+        alert("Błąd w trakcie zapisywania oferty");
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
